59777b6efbf2008d90ecd233d8c285d04ad1395a
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-12 19:06:50 +0545

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,107 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+// import { Icons } from './Icons'
+import NavItems from './NavItems'
+// import { buttonVariants } from './ui/button'
+// import Cart from './Cart'
+// import { getServerSideUser } from '@/lib/payload-utils'
+// import { cookies } from 'next/headers'
+// import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+// import NavItems from './NavItems'
+// import { Icons } from './ui/icons'
+// import UserAccountNav from './UserAccountNav'
+// import MobileNav from './MobileNav'
+
+const Navbar =  () => {
+  // const nextCookies = cookies()
+//   const { user } = await getServerSideUser(nextCookies)
+
+  return (
+    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+      <header className='relative bg-white'>
+        <MaxWidthWrapper>
+          <div className='border-b border-gray-200'>
+            <div className='flex h-16 items-center'>
+              {/* <MobileNav /> */}
+
+              <div className='ml-4 flex lg:ml-0 font-semibold'>
+                <Link href='/'>
+                  {/* <Icons.logo className='h-10 w-10' />
+                  <p>this is if u wanna use svg as icon</p> */}
+                 <p className='ml-4 flex lg:ml-0'> Unwhispered<span className="text-green-600">Perhaps..</span>
+                  </p></Link>
+              </div>
+
+              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                <NavItems />
+              </div>
+
+              {/* <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
+
+// function getServerSideUser(nextCookies: ReadonlyRequestCookies): { user: any } | PromiseLike<{ user: any }> {
+//     throw new Error('Function not implemented.')
+// }
+// function getServerSideUser(nextCookies: ReadonlyRequestCookies): { user: any } | PromiseLike<{ user: any }> {
+//     throw new Error('Function not implemented.')
+// }
+
