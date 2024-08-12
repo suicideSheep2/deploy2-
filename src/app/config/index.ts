b838d9b6cb995de62c3679318df96fb1dbdcfb77
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-12 22:59:04 +0545

    changed button content bestfit

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,41 +1,41 @@
 export const PRODUCT_CATEGORIES = [
     {
-      label: 'UI Kits',
+      label: 'Poems',
       value: 'ui_kits' as const,
       featured: [
         {
-          name: 'Editor picks',
+          name: 'Editor Picks',
           href: `/products?category=ui_kits`,
           imageSrc: '/nav/ui-kits/mixed.jpg',
         },
         {
-          name: 'New Arrivals',
+          name: 'Recently Added',
           href: '/products?category=ui_kits&sort=desc',
           imageSrc: '/nav/ui-kits/blue.jpg',
         },
         {
-          name: 'Bestsellers',
+          name: 'Best-rated',
           href: '/products?category=ui_kits',
           imageSrc: '/nav/ui-kits/purple.jpg',
         },
       ],
     },
     {
-      label: 'Icons',
+      label: 'Novels',
       value: 'icons' as const,
       featured: [
         {
-          name: 'Favorite Icon Picks',
+          name: 'Editor Picks',
           href: `/products?category=icons`,
           imageSrc: '/nav/icons/picks.jpg',
         },
         {
-          name: 'New Arrivals',
+          name: 'Recently Added',
           href: '/products?category=icons&sort=desc',
           imageSrc: '/nav/icons/new.jpg',
         },
         {
-          name: 'Bestselling Icons',
+          name: 'Best-rated',
           href: '/products?category=icons',
           imageSrc: '/nav/icons/bestsellers.jpg',
         },
