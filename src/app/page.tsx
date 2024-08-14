fcff2574314e251ee3780e1a771c1da2db9c76de
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-14 22:22:06 +0545

    modyfing landing page context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import Link from 'next/link'
 const perks = [
   {
     // make it ai kinda interaction
-    name: 'Instant Delivery',
+    name: 'Creative Space',
     Icon: ArrowDownToLine,
     
     description:
@@ -43,16 +43,15 @@ export default function Home() {
       <MaxWidthWrapper>
         <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
           <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-            Your marketplace for high-quality{' '}
+            Your platform for literary-expression{' '}
             <span className='text-blue-600'>
-              digital assets
+              and discovery
             </span>
             .
           </h1>
           <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
-            Welcome to UnwrittenPerhaps. Every asset on our
-            platform is focused for best user experience on exploring to publishing 
-            literature items.
+          From AI-sparked verses to epic sagas, craft your literary legacy on a platform 
+          that turns readers into writers and dreams into digital ink."
           </p>
           <div className='flex flex-col sm:flex-row gap-4 mt-6'>
             <Link
