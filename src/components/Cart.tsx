4d396028819678acf14e63299b1512fdf4e99dd4
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-14 18:42:10 +0545

    working on sidebar

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -10,7 +10,7 @@ import {
   SheetTrigger,
 } from './ui/sheet'
 
-// import { Separator } from './ui/separator'
+import { Separator } from './ui/separator'
 // import { formatPrice } from '@/lib/utils'
 import Link from 'next/link'
 import { buttonVariants } from './ui/button'
@@ -19,24 +19,12 @@ import Image from 'next/image'
 // import { ScrollArea } from './ui/scroll-area'
 // import CartItem from './CartItem'
 import { useEffect, useState } from 'react'
+import { formatPrice } from '@/lib/utils'
 
 const Cart = () => {
-  // const { items } = useCart()
-  // const itemCount = items.length
+  const itemCount = 1 
 
-  // const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  // useEffect(() => {
-  //   setIsMounted(true)
-  // }, [])
-
-  // const cartTotal = items.reduce(
-  //   (total, { product }) => total + product.price,
-  //   0
-  // )
-
-  // const fee = 1
-
+  
   return (
     <Sheet>
       <SheetTrigger className='group -m-2 flex items-center p-2'>
@@ -45,25 +33,19 @@ const Cart = () => {
           className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
         />
         <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
-          {/* {isMounted ? itemCount : 0} */}
+        0
         </span>
       </SheetTrigger>
       <SheetContent className='flex w-full flex-col pr-0 sm:max-w-lg'>
         <SheetHeader className='space-y-2.5 pr-6'>
         <SheetTitle>Cart (0)</SheetTitle>
-          {/* <SheetTitle>Cart ({itemCount})</SheetTitle> */}
         </SheetHeader>
-        {/* {itemCount > 0 ? (
+       
+         {itemCount > 0 ? (
           <>
             <div className='flex w-full flex-col pr-6'>
-              <ScrollArea>
-                {items.map(({ product }) => (
-                  <CartItem
-                    product={product}
-                    key={product.id}
-                  />
-                ))}
-              </ScrollArea>
+             {/* todo : cart logic */}
+             cart items
             </div>
             <div className='space-y-4 pr-6'>
               <Separator />
@@ -76,57 +58,16 @@ const Cart = () => {
                   <span className='flex-1'>
                     Transaction Fee
                   </span>
-                  <span>{formatPrice(fee)}</span>
-                </div>
-                <div className='flex'>
-                  <span className='flex-1'>Total</span>
-                  <span>
-                    {formatPrice(cartTotal + fee)}
-                  </span>
+                {/* no need for me tbh */}
+                  <span>{formatPrice(1)}</span>
                 </div>
               </div>
+              </div>
 
-              <SheetFooter>
-                <SheetTrigger asChild>
-                  <Link
-                    href='/cart'
-                    className={buttonVariants({
-                      className: 'w-full',
-                    })}>
-                    Continue to Checkout
-                  </Link>
-                </SheetTrigger>
-              </SheetFooter>
-            </div>
           </>
         ) : (
-          <div className='flex h-full flex-col items-center justify-center space-y-1'>
-            <div
-              aria-hidden='true'
-              className='relative mb-4 h-60 w-60 text-muted-foreground'>
-              <Image
-                src='/hippo-empty-cart.png'
-                fill
-                alt='empty shopping cart hippo'
-              />
-            </div>
-            <div className='text-xl font-semibold'>
-              Your cart is empty
-            </div>
-            <SheetTrigger asChild>
-              <Link
-                href='/products'
-                className={buttonVariants({
-                  variant: 'link',
-                  size: 'sm',
-                  className:
-                    'text-sm text-muted-foreground',
-                })}>
-                Add items to your cart to checkout
-              </Link>
-            </SheetTrigger>
-          </div>
-        )} */}
+      <div> </div>
+        )}
       </SheetContent>
     </Sheet>
   )
