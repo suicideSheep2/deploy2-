d9634f4d210e7ff293f2e1439ddf4459ed8690b1
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-15 10:07:55 +0545

    working on local dependencies

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,10 @@
 
 // to run locally
-import {mongooseAdapter} from 'payload/config'
+import {mongooseAdapter} from '@payloadcms/db-mongodb'
 import { buildConfig } from "payload/config"
+import { slateEditor } from '@payloadcms/richtext-slate'
+import  { webpackBundler } from '@payloadcms/bundler-webpack'
+import path from 'path'
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '', 
@@ -9,9 +12,22 @@ export default buildConfig({
     routes: {
         admin:'/sell'
     },
-    admin: {},
+    admin: {
+        bundler: webpackBundler(),
+        meta:{
+            titleSuffix: "-DigitalHippo",
+            favicon: 'favicon.ico',
+            ogImage: '/thumbnail.jpg'
+        },
+    },
+    rateLimit: {
+    max: 2000,
+    },
     editor: slateEditor ({}),
-    db:mongooseAdapter({
+    db: mongooseAdapter({
         url:process.env.MONGODB_URL!,
     }),
+    typescript: {
+        outputFile : path.resolve(__dirname, 'payload-types.ts')
+    },
 })
