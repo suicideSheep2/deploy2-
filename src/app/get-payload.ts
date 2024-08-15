a27820ae93677015f149d3773c71d470e79ddc8e
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-15 08:52:53 +0545

    *$

--- src/app/get-payload.ts
+++ src/app/get-payload.ts
@@ -1,8 +1,9 @@
-
+// created a database client 
 import dotenv from 'dotenv'
 import path from 'path'
 import { cache } from 'react'
 import type {InitOptions} from 'payload/config'
+import payload from 'payload'
 
 dotenv.config({
     path:path.resolve(__dirname, "../.env")
@@ -26,4 +27,22 @@ export const getPayloadClient = async ({
     if (!process.env.PAYLOAD_SECRET) {
         throw new Error ('PAYLOAD_SECRET is missing')
     }
+    if( cached.client) {
+        return cached.client
+    }
+
+    if(!cached.promise) {
+        cached.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local:initOptions?.express ? false :true,
+            ...(initOptions || {}),
+        })
+    }
+    try {
+        cached.client = await cached.promise 
+    } catch(e:unknown) {
+        cached.promise = null
+        throw e
+    }
+    return cached.client
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 // this is for admin dashboard
 import express from 'express'
+import { getPayloadClient } from './app/get-payload'
 
 
 const app = express()
-const PORT = Number(processs.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 const start = async () => {
-    const payload = await getPayloadClient()
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info('Admin URL ${cms.getAdminURL()}')
+            },
+        },
+    })
 
 }
 
