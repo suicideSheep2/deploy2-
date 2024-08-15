8d1206aa350bf677e10d65615ebd4419339a2be3
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-15 09:30:52 +0545

    aadding dependencies

---
+++ src/payload.config.ts
@@ -0,0 +1,17 @@
+
+// to run locally
+import {mongooseAdapter} from 'payload/config'
+import { buildConfig } from "payload/config"
+
+export default buildConfig({
+    serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '', 
+    collections: [],
+    routes: {
+        admin:'/sell'
+    },
+    admin: {},
+    editor: slateEditor ({}),
+    db:mongooseAdapter({
+        url:process.env.MONGODB_URL!,
+    }),
+})
