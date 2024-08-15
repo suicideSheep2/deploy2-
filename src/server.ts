009f304e4409503aaf0431e3fec8aad822050d3d
Bhabuk Bh <ftraveller23@gmail.com>
2024-08-15 08:43:04 +0545

    working on admin dashboard *2

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
 // this is for admin dashboard
+import express from 'express'
+
+
+const app = express()
+const PORT = Number(processs.env.PORT) || 3000
+
+const start = async () => {
+    const payload = await getPayloadClient()
+
+}
+
+start()
+
