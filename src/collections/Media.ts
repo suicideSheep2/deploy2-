b8deab4c99cf56f3f87acc0d1be9a254eeb3f7c2
Bhabuk _B <ftraveller23@gmail.com>
2024-08-24 15:06:13 +0545

    working on who has access to images to view

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,20 +1,20 @@
 import { User } from '../payload-types'
 import { Access, CollectionConfig } from 'payload/types'
 
-// const isAdminOrHasAccessToImages =
-//   (): Access =>
-//   async ({ req }) => {
-//     const user = req.user as User | undefined
+const isAdminOrHasAccessToImages =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined
 
-//     if (!user) return false
-//     if (user.role === 'admin') return true
+    if (!user) return false
+    if (user.role === 'admin') return true
 
-//     return {
-//       user: {
-//         equals: req.user.id,
-//       },
-//     }
-//   }
+    return {
+      user: {
+        equals: req.user.id,
+      },
+    }
+  }
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -25,19 +25,19 @@ export const Media: CollectionConfig = {
       },
     ],
   },
-//   access: {
-//     read: async ({ req }) => {
-//       const referer = req.headers.referer
+  access: {
+    read: async ({ req }) => {
+      const referer = req.headers.referer
 
-//       if (!req.user || !referer?.includes('sell')) {
-//         return true
-//       }
+      if (!req.user || !referer?.includes('sell')) {
+        return true
+      }
 
-//       return await isAdminOrHasAccessToImages()({ req })
-//     },
-//     delete: isAdminOrHasAccessToImages(),
-//     update: isAdminOrHasAccessToImages(),
-//   },
+      return await isAdminOrHasAccessToImages()({ req })
+    },
+    delete: isAdminOrHasAccessToImages(),
+    update: isAdminOrHasAccessToImages(),
+  },
   admin: {
     hidden: ({ user }) => user.role !== 'admin',
   },
