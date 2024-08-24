ea824f8abb72226cd1a612407993f60327494c03
Bhabuk _B <ftraveller23@gmail.com>
2024-08-24 20:34:11 +0545

    sucessefully shown imagesin homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import ProductReel from "@/components/ProductReel"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { ArrowDownToLine, CheckCircle, Leaf, Link } from "lucide-react"
 
@@ -67,7 +68,11 @@ export default function Home() {
           </div>
         </div>
 
-      
+        <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          href='/products?sort=recent'
+          title='Brand new'
+        />
       </MaxWidthWrapper>
 
       <section className='border-t border-gray-200 bg-gray-50'>
