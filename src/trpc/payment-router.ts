af05b7bf02f843986af11b809030b0f05397b79b
Bhabuk _B <ftraveller23@gmail.com>
2024-08-26 01:52:48 +0545

    same shit

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,6 +1,26 @@
-import { router } from "./trpc"
+import { z } from "zod"
+import { privateProcedure, router } from './trpc'
+import { TRPCError } from '@trpc/server'
+import { getPayload } from 'payload'
+import { getPayloadClient } from '../get-payload'
  
 
 export const paymentRouter = router({
+    createSession: privateProcedure
+    .input(z.object ({ productIds: z.array(z.string())}))
+    .mutation(async({ctx, input }) => {
+        const {user} = ctx
+        let {productIds } = input
+
+// this cheeeks if the cart is empty doesn't allows us to pay
+// need t oremove this restriction so user can pay freely ?? 
+// huh sth along the line ....for later yll ffff
+        if(productIds.length == 0){
+            throw new TRPCError({code: 'BAD_REQUEST'})
+        }
+
+        const payload = await getPayloadClient()
+        
+    }),
 
 })
