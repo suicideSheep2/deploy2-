6bdef8dc58f782ddf4b62b98630d9f11b262232e
Bhabuk _B <ftraveller23@gmail.com>
2024-08-25 21:37:56 +0545

    product page with adding and similar category addition

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -134,8 +134,43 @@ const Page = async ({ params }: PageProps) => {
             </div>
           </div>
 
+         {/* add to cart part
+         make it add to favourites
+         or sth like that btt okay for now */}
+         <div className='mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start'>
+            <div>
+              <div className='mt-10'>
+                {/* <AddToCartButton product={product} /> */}
+              </div>
+              <div className='mt-6 text-center'>
+                <div className='group inline-flex text-sm text-medium'>
+                  <Shield
+                    aria-hidden='true'
+                    className='mr-2 h-5 w-5 flex-shrink-0 text-gray-400'
+                  />
+                  <span className='text-muted-foreground hover:text-gray-700'>
+                    30 Day Return Guarantee
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div> 
+
+
         </div>
       </div>
+
+      <ProductReel
+        href='/products'
+        query={{ category: product.category, limit: 4 }}
+        title={`Similar ${label}`}
+         //this label gives 'poems'/'novel' on screen
+        //  but maybe just say Similar Products/ Contents directly ??
+        // obv for later modification
+
+        // modify it a bit just on language
+        subtitle={`Browse similar contents ${label} just like '${product.name}'`}
+      />
   </MaxWidthWrapper>
  )
 }
