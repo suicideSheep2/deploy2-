0948752cc2a95c0a2a6af248ff54e007144827ed
Bhabuk _B <ftraveller23@gmail.com>
2024-08-25 22:31:30 +0545

    added functionality to add things/ products to cart/favourites

---
+++ src/hooks/use-cart.ts
@@ -0,0 +1,43 @@
+// do we need to midify it  ?? i think no cuz it just adds products to cart 
+// maybe jsut desciption and some visuals but let it leave for now 
+
+import { Product } from '@/payload-types'
+import { create } from 'zustand'
+import {
+  createJSONStorage,
+  persist,
+} from 'zustand/middleware'
+
+export type CartItem = {
+  product: Product
+}
+
+type CartState = {
+  items: CartItem[]
+  addItem: (product: Product) => void
+  removeItem: (productId: string) => void
+  clearCart: () => void
+}
+
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          return { items: [...state.items, { product }] }
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter(
+            (item) => item.product.id !== id
+          ),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: 'cart-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
