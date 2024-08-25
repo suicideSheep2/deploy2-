203f93237a1b65174328cbe0cb4ceed8eabb08fe
Bhabuk _B <ftraveller23@gmail.com>
2024-08-26 03:09:10 +0545

    working incomplete with stripe

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -3,6 +3,7 @@ import { privateProcedure, router } from './trpc'
 import { TRPCError } from '@trpc/server'
 import { getPayload } from 'payload'
 import { getPayloadClient } from '../get-payload'
+import { stripe } from "@/lib/stripe"
  
 
 export const paymentRouter = router({
@@ -33,6 +34,49 @@ export const paymentRouter = router({
               },
             },
           })
+
+          const filteredProducts = products.filter((prod) =>
+            Boolean(prod.priceId)
+          )
+
+          const order = await payload.create({
+            collection: 'orders',
+            data: {
+              _isPaid: false,
+            //   needs to be changed
+            // cuz we're imagining user to just 
+            // donate whatever amount they want
+              products: filteredProducts.map((prod) => prod.id),
+              user: user.id,
+            },
+          })
+
+          const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
+        []
+
+        line_items.push({
+            // fk can't make stripe id from nepal
+            price: "This_is_stripe_made_balance_id_code"
+        })
+        // transaction fee
+
+          try {
+            const stripeSession =
+              await stripe.checkout.sessions.create({
+                // tf needs to be modified a bit 
+                // just a bit of word manipulation tbh
+                success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+                cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+                //maybe some nepali payments too ?? 
+                // sounds too good to be true
+                payment_method_types: ['card', 'paypal'], 
+                mode: 'payment',
+                metadata: {
+                  userId: user.id,
+                  orderId: order.id,
+                },
+                line_items,
+              })
 
     }),
 
