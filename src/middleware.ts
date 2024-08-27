434aac23d60b450d2974156150c9966aeaa7df6b
Bhabuk _B <ftraveller23@gmail.com>
2024-08-27 13:31:47 +0545

    added webhooks

---
+++ src/middleware.ts
@@ -0,0 +1,3 @@
+// if user is logged in
+// user should't be able to see login button 
+// and vice versa
--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,113 @@
-import express from 'express';
-
-import {getPayloadClient} from './get-payload'
-import { nextApp, nextHandler } from "./next-utils"
-import * as trpcExpress from "@trpc/server/adapters/express"
-import { appRouter } from './trpc';
-import { inferAsyncReturnType } from '@trpc/server';
-
-// here must be some security fixes
-// mainly to use payment one must be signed in 
-// kinda odd for my vision btw think about it later
+import express from 'express'
+import { getPayloadClient } from './get-payload'
+import { nextApp, nextHandler } from './next-utils'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { appRouter } from './trpc'
+import { inferAsyncReturnType } from '@trpc/server'
+import bodyParser from 'body-parser'
+import { IncomingMessage } from 'http'
+import { stripeWebhookHandler } from './webhooks'
+import nextBuild from 'next/dist/build'
+import path from 'path'
+import { PayloadRequest } from 'payload/types'
+import { parse } from 'url'
 
 const app = express()
-const PORT  = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 3000
 
-const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({
-    req,
-    res,
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
 })
-export type ExpressContext = inferAsyncReturnType<typeof createContext>
+
+export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
+
+export type WebhookRequest = IncomingMessage & {
+  rawBody: Buffer
+}
 
 const start = async () => {
-    
-    const payload = await getPayloadClient({
-        initOptions: {
-            express: app,
-            onInit: async (cms) => {
-                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
-            },
-        },
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer) => {
+      req.rawBody = buffer
+    },
+  })
+
+  app.post(
+    '/api/webhooks/stripe',
+    webhookMiddleware,
+    stripeWebhookHandler
+  )
+
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+      },
+    },
+  })
+//   you have to be logged in to go to cart page 
+// code implementation
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        'Next.js is building for production'
+      )
+
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+
+      process.exit()
     })
-    
-app.use(
+
+    return
+  }
+// is this in wrong place uff? fk works leave it
+
+
+  const cartRouter = express.Router()
+
+  cartRouter.use(payload.authenticate)
+
+  cartRouter.get('/', (req, res) => {
+    const request = req as PayloadRequest
+
+    if (!request.user)
+      return res.redirect('/sign-in?origin=cart')
+
+    const parsedUrl = parse(req.url, true)
+    const { query } = parsedUrl
+
+    return nextApp.render(req, res, '/cart',parsedUrl.query)
+  })
+
+  app.use('/cart', cartRouter)
+  app.use(
     '/api/trpc',
-     trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-}))
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+      createContext,
+    })
+  )
 
-   app.use((req, res) =>  nextHandler(req, res))
+  app.use((req, res) => nextHandler(req, res))
 
-   nextApp.prepare().then(() => {
+  nextApp.prepare().then(() => {
     payload.logger.info('Next.js started')
 
     app.listen(PORT, async () => {
-        payload.logger.info(
-            `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      )
     })
-   })
-
+  })
 }
 
 start()
