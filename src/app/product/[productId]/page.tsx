58f658240cd1a72c2177d33e986d2acbe9b391e5
Bhabuk _B <ftraveller23@gmail.com>
2024-08-29 14:59:50 +0545

    2nd page alyought done for description

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -71,14 +71,7 @@ const Page = async ({ params }: PageProps) => {
     <MaxWidthWrapper className='bg-white'>
       <div className='bg-white'>
         <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
-          {/* Product images - moved to the top */}
-          <div className='lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-start'>
-            <div className='aspect-square rounded-lg'>
-              <ImageSlider urls={validUrls} />
-            </div>
-          </div>
-
-          {/* Product Details - adjusted to start from the left */}
+          {/* Product Details */}
           <div className='lg:col-start-1 lg:row-start-1 lg:max-w-lg lg:self-start'>
             <ol className='flex items-center space-x-2'>
               {BREADCRUMBS.map((breadcrumb, i) => (
@@ -101,13 +94,21 @@ const Page = async ({ params }: PageProps) => {
                 </div>
               </li>
               ))}
-          </ol>
+            </ol>
 
-          <div className='mt-4'>
+            <div className='mt-4'>
               <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
               {product.name}
               </h1>
             </div>
+
+            {/* Product Images - Now after the title */}
+            <div className='mt-6 mb-8 lg:hidden'>
+              <div className='aspect-square rounded-lg'>
+                <ImageSlider urls={validUrls} />
+              </div>
+            </div>
+
             <section className='mt-4'>
               <div className='flex items-center'>
                 <p className='font-medium text-gray-900'>
@@ -127,22 +128,22 @@ const Page = async ({ params }: PageProps) => {
               </div>
 
               <div className='mt-6 flex items-center'>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 text-muted-foreground"
-                >
-                  <path d="M10 9a3 3 0 100-6 3 3 0 000 6zM6 8a2 2 0 11-4 0 2 2 0 014 0zM1.49 15.326a.78.78 0 01-.358-.442 3 3 0 014.308-3.516 6.484 6.484 0 00-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 01-2.07-.655zM16.44 15.98a4.97 4.97 0 002.07-.654.78.78 0 00.357-.442 3 3 0 00-4.308-3.517 6.484 6.484 0 011.907 3.96 2.32 2.32 0 01-.026.654zM18 8a2 2 0 11-4 0 2 2 0 014 0zM5.304 16.19a.844.844 0 01-.277-.71 5 5 0 019.947 0 .843.843 0 01-.277.71A6.975 6.975 0 0110 18a6.974 6.974 0 01-4.696-1.81z" />
-                </svg>
+                <span className='text-muted-foreground'>©</span>
                 <p className='ml-2 text-sm text-muted-foreground'>
-                  By {product.author}
+                  {product.author}
                 </p>
               </div>
             </section>
           </div>
 
-          {/* Add to cart part - moved below product details */}
+          {/* Product Images - For large screens */}
+          <div className='hidden lg:block lg:col-start-2 lg:row-span-2 lg:self-start'>
+            <div className='aspect-square rounded-lg'>
+              <ImageSlider urls={validUrls} />
+            </div>
+          </div>
+
+          {/* Add to cart part */}
           <div className='mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start'>
             <div>
               <div className='mt-10'>
