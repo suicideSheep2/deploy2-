19b7bfad1c52d1e5684fab5ec9fca8c05e01a6ea
Bhabuk _B <ftraveller23@gmail.com>
2024-08-30 12:14:12 +0545

    working on fav button

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button } from './ui/button'
 import { useCart } from '@/hooks/use-cart'
 import { Product } from '@/payload-types'
+import { Star } from 'lucide-react'
 
-const AddToCartButton = ({
+const AddToFavoritesButton = ({
   product,
 }: {
   product: Product
@@ -13,25 +14,27 @@ const AddToCartButton = ({
   const { addItem } = useCart()
   const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsSuccess(false)
-    }, 2000)
-
-    return () => clearTimeout(timeout)
-  }, [isSuccess])
-
   return (
     <Button
       onClick={() => {
         addItem(product)
         setIsSuccess(true)
+        setTimeout(() => setIsSuccess(false), 2000)
       }}
-      size='lg'
-      className='w-full'>
-      {isSuccess ? 'Added!' : 'Add to cart'}
+      size='sm'
+      variant='ghost'
+      className='group relative'
+      title='Add to Favorites'>
+      <Star
+        className={`h-5 w-5 transition-colors ${
+          isSuccess ? 'fill-yellow-400 text-yellow-400' : 'text-gray-400 group-hover:text-yellow-400'
+        }`}
+      />
+      <span className='absolute -top-8 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 transition-opacity group-hover:opacity-100'>
+        {isSuccess ? 'Added to Favorites!' : 'Add to Favorites'}
+      </span>
     </Button>
   )
 }
 
-export default AddToCartButton
+export default AddToFavoritesButton
