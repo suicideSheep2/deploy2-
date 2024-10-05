9396c0323f25c61ced266954aa1a743a4b7e8bd3
Bhabuk _B <ftraveller23@gmail.com>
2024-10-05 07:05:01 +0545

    morning debug

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -71,29 +71,14 @@ export const Products: CollectionConfig = {
       type: 'richText',
       editor: lexicalEditor({
         features: ({ defaultFeatures }) => [
-          ...defaultFeatures,
-          HTMLConverterFeature({}),
+          // ...defaultFeatures,
+          // HTMLConverterFeature({}),
         ],
       }),
-      admin: {
-        // elements: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'ol', 'ul', 'link'],
-        // leaves: ['bold', 'italic', 'underline', 'strikethrough'],
-      },
     },
     {
       name: 'description_html',
-      type: 'textarea',  // Changed from 'text' to 'textarea' for larger content
-      hooks: {
-        beforeChange: [
-          ({ value, originalDoc }) => {
-            // Don't override existing value if no new value is provided
-            if (!value && originalDoc?.description_html) {
-              return originalDoc.description_html;
-            }
-            return value;
-          },
-        ],
-      },
+      type: 'textarea', //changed
       admin: {
         hidden: true,
       },
